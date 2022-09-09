2a7d313ba5b52b4acab0e008a2744d6c77d65b68
DimShadoWWW <DimShadoWWW@users.noreply.github.com>
2022-09-09 17:18:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# streamlit-dataimport-test
---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+# uploaded_file = st.file_uploader('Choose a file')
+
+# st.write(dir(uploaded_file))
+# st.write(uploaded_file)
+
+# # if uploaded_file is not None:
+# #     st.write(dir(uploaded_file))
+# #     st.write(uploaded_file)
+# #     #read csv
+# #     # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
+    
