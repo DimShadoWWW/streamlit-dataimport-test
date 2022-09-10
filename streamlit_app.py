c48ef3539d912ad6eb0a7e12db65006ebe5e2b03
Anielkis Herrera <aherrera@zato.io>
2022-09-10 09:54:59 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,35 @@ def new_field(name):
     if st.checkbox('custom'):
         v = st.text_input('custom name')
     return v
-    
+
+columns_rename = {}
+# if 'columns_data' not in st.session_state.keys():
+#     st.session_state['columns_data'] = columns_data
+# else:
+#     columns_data = st.session_state['columns_data']
+
+def checkbox_container(data):
+    st.header('Add renaming of fields')
+
+    cols = st.columns(10)
+    if cols[0].button("New", help="Add new rename of columns"):
+        c = str(st.selectbox('Input field name', options=[d for d in dest_columns if d not in columns_rename.keys()]))
+        columns_rename[c]= new_field(c)
+
+    # if cols[1].button('Select All'):
+    #     for i in data:
+    #         st.session_state['dynamic_checkbox_' + i] = True
+    #     st.experimental_rerun()
+    # if cols[2].button('UnSelect All'):
+    #     for i in data:
+    #         st.session_state['dynamic_checkbox_' + i] = False
+    #     st.experimental_rerun()
+    # for i in data:
+    #     st.checkbox(i, key='dynamic_checkbox_' + i)
+
+# def get_selected_checkboxes():
+#     return [i.replace('dynamic_checkbox_','') for i in st.session_state.keys() if i.startswith('dynamic_checkbox_') and st.session_state[i]]
+
 
 if uploaded_file is not None:
     # st.write(dir(uploaded_file))
@@ -23,20 +51,27 @@ if uploaded_file is not None:
     df = pd.read_csv(StringIO('\n'.join([l.decode().replace('\n', '') for l in lines])))
     st.write(df)
     # st.write(df.columns)
-    columns_rename = {}
+    # columns_rename = {}
     # first_column = df.columns.tolist()
     # c: '' for c in df.columns.tolist()}
     # for c in columns_rename.keys():
     #     columns_rename[c] = st.selectbox(label=c, options=dest_columns)
 
+    checkbox_container(df.columns.tolist())
+
     st.json(columns_rename)
     
     st.write(df.rename(columns=columns_rename))
-    #read csv
-    # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
-    while True:
-        c = st.selectbox('Rename field', options=[d for d in dest_columns if d not in columns_rename.keys()])
-        # c = st.selectbox('Rename field', options=[d for d in dest_columns if d not in columns_rename.keys()])
-        if st.button("New", help="Add new rename of columns"):
-            columns_rename[str(c)] = new_field(str(c))
+    # #read csv
+    # # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
+    # while True:
+    #     # c = st.selectbox('Rename field', options=[d for d in dest_columns if d not in columns_rename.keys()])
+    #     if st.button("New", help="Add new rename of columns"):
+    #         c = st.selectbox('Rename field', options=[d for d in dest_columns if d not in columns_rename.keys()])
+    #         columns_rename[str(c)] = new_field(str(c))
         
+
+
+checkbox_container(columns_data)
+st.write('You selected:')
+st.write(get_selected_checkboxes())
