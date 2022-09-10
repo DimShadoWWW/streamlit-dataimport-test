2d28f596ebc07d88c70936946bb10d851a28d7b0
Anielkis Herrera <aherrera@zato.io>
2022-09-09 20:26:10 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,10 @@ uploaded_file = st.file_uploader('Choose a file')
 if uploaded_file is not None:
     st.write(dir(uploaded_file))
     st.write(uploaded_file)
+    lines = uploaded_file.readlines()[:3]
+    df = pd.read_csv(pd.compat.StringIO('\n'.join(lines)), header=None)
+    st.write(df)
+    st.write(df.columns)
     #read csv
     # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
     
