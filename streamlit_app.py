519b2a458fee8aeebaf945dbb651c039d6665618
Anielkis Herrera <aherrera@zato.io>
2022-09-09 20:30:12 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ if uploaded_file is not None:
     st.write(dir(uploaded_file))
     st.write(uploaded_file)
     lines = uploaded_file.readlines()[:3]
-    df = pd.read_csv(StringIO('\n'.join([l.decode().replace('\n', '') for l in lines])), header=None)
+    df = pd.read_csv(StringIO('\n'.join([l.decode().replace('\n', '') for l in lines])))
     st.write(df)
     st.write(df.columns)
     #read csv
