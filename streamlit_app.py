b3efcff66eac18c13d3d1d22ec134bbdfc3e4233
Anielkis Herrera <aherrera@zato.io>
2022-09-09 20:22:36 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,9 @@ import numpy as np
 
 uploaded_file = st.file_uploader('Choose a file')
 
-st.write(dir(uploaded_file))
-st.write(uploaded_file)
-
-# if uploaded_file is not None:
-#     st.write(dir(uploaded_file))
-#     st.write(uploaded_file)
-#     #read csv
-#     # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
+if uploaded_file is not None:
+    st.write(dir(uploaded_file))
+    st.write(uploaded_file)
+    #read csv
+    # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
     
