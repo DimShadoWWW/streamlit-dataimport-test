512e47497e6b1ac037252e525af02abf36874a74
Anielkis Herrera <aherrera@zato.io>
2022-09-10 17:53:02 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ if uploaded_file is not None:
     # st.write(uploaded_file)
     lines = uploaded_file.readlines()[:3]
     df = pd.read_csv(StringIO('\n'.join([l.decode().replace('\n', '') for l in lines])))
+    st.text('datos originales')
     st.write(df)
     # st.write(df.columns)
     columns_rename = {c: c for c in dict.fromkeys(df.columns.tolist()).keys()}
@@ -30,8 +31,10 @@ if uploaded_file is not None:
                 columns_rename[c] = v[c]
         cl = cl + 1
 
+    st.text('renombrado de columns')
     st.write(columns_rename)
     
+    st.text('datos renombrados')
     st.write(df.rename(columns=columns_rename))
     #read csv
     # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
