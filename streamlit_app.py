3512a0086ebcabf58dba5ac29e44c1a2b20776c9
Anielkis Herrera <aherrera@zato.io>
2022-09-10 21:47:34 -0500

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,60 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import StringIO
+import uuid
+import redis
+from furl import furl
+from redis.commands.json.path import Path
+from redis.commands.search.field import NumericField, TagField, TextField
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+from redis.commands.search.query import NumericFilter, Query
+
+redis_url = furl(st.secrets["redis_url"])
+
+redisCon = redis.Redis(
+    host=redis_url.host,
+    port=redis_url.port,
+    password=redis_url.password if redis_url.password != '' else None,
+    # host='redis',
+    # port=6379,
+    # host='aqueous-badlands-51618.herokuapp.com',
+    # port=80,
+    # password='af80e90f144cdd37ae539349ba0bc7a4',
+    max_connections=10,
+    decode_responses=True,
+    retry_on_timeout=True)
+
+index_name = "idx:data"
+
+schema = (
+    TextField("$.name", as_name="name", sortable=True),
+    # TextField("$.middleName", as_name="middleName", sortable=True),
+    # TextField("$.lastName", as_name="lastName", sortable=True),
+    # TextField("$.shortName", as_name="shortName", sortable=True),
+    # TextField("$.nickName", as_name="nickName", sortable=True),
+    # TextField("$.side", as_name="side", sortable=True),
+    # TextField("$.country", as_name="country", sortable=True),
+
+    NumericField("$.id", as_name="id"),
+    # NumericField("$.countryId", as_name="countryId"),
+    # NumericField("$.teamId", as_name="teamId"),
+    # NumericField("$.currentTeamId", as_name="currentTeamId"),
+    # NumericField("$.gamesPlayed", as_name="gamesPlayed", sortable=True),
+
+    # # TagField("$.user.city", as_name="city"),
+    # TagField("$.channel", as_name="channel"),
+)
+
+try:
+    ftindex_data = redisCon.ft(index_name)
+    ftindex_data.create_index(schema,
+                                definition=IndexDefinition(
+                                    prefix=["data:"],
+                                    index_type=IndexType.JSON))
+except redis.exceptions.ResponseError:
+    pass
+
+
 
 uploaded_file = st.file_uploader('Choose a file')
 
@@ -35,7 +89,16 @@ if uploaded_file is not None:
     st.json(columns_rename)
     
     st.text('datos renombrados')
-    st.write(df.rename(columns=columns_rename))
+    df = df.rename(columns=columns_rename)
+    st.write(df)
     #read csv
     # df1=pd.read_csv(uploaded_file, type=['csv','xlsx'], accept_multiple_files=False)
     
+    if st.button('import'):
+        df['uuid'] = [uuid.uuid4() for x in range(df.shape[0])]
+        for index, row in df.iterrows():
+            values = row.to_dict()
+            for k in values.keys():
+                print(k, type(values[k]))
+            print("data:{id}".format(id=values['uuid']))
+            redisCon.json().set("data:{id}".format(id=values['uuid']), '.', values)
